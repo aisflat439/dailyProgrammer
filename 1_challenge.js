c99d7829898dc25e73f88820908a7e9c2c8bb164
aisflat439 <fitzsimonsdevin@gmail.com>
2017-02-24 08:57:45 -0500

    fizzbuzz solution

--- 1_challenge.js
+++ 1_challenge.js
@@ -1,3 +1,42 @@
 'use strict'
 
-console.log("It works!");
+// Today's challenge is FizzBuzz.
+// Since you know it, it should be a good starting point.
+
+function fizzBuzz(value) {
+  let output = "";
+
+  if (value === 0){
+    return output += "You entered zero";
+  }
+
+  if (value < 0){
+    for (let i = value; i < 0; i++){
+      output += "\n" + i + " ";
+      output += modulusCheck(3, i);
+      output += modulusCheck(5, i, "Buzz");
+    }
+  } else {
+    for (let i = 1; i <= value; i++){
+      output += "\n" + i + " ";
+      output += modulusCheck(3, i);
+      output += modulusCheck(5, i, "Buzz");
+    }
+  }
+
+  return output;
+}
+
+function modulusCheck(valueToCheckAgainst, numberBeingChecked, outputDesired = "Fizz"){
+  let output = "";
+
+  if (numberBeingChecked % valueToCheckAgainst === 0){
+    output += outputDesired;
+  }
+
+  return output;
+}
+
+console.log(fizzBuzz(0));
+console.log(fizzBuzz(100));
+console.log(fizzBuzz(-100));

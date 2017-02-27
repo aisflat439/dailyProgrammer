49a4d2c12dd78ed6f7ed28d53a0636d7c52e1806
aisflat439 <fitzsimonsdevin@gmail.com>
2017-02-27 11:29:22 -0500

    start of array challenge

---
+++ february/2_challenge.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// // merge two sorted arrays into one sorted arrays
+// Merge the following two sorted arrays into one sorted array.
+//
+// arrayOne = [ 2, 9, 22, 97, 167, 204 ]
+// arrayTwo = [ 3, 7, 10, 22, 98, 188, 203, 205, 209, 215]
+
+var ARRAY_ONE = [2, 9, 22, 97, 167, 204];
+var ARRAY_TWO = [3, 7, 10, 22, 98, 188, 203, 205, 209, 215];
+
+function mergeTwoArrays(arrayOne, arrayTwo){
+  var mergedArray = []
+  return mergedArray;
+}
+
+mergeTwoArrays(ARRAY_ONE, ARRAY_TWO);

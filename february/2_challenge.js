94528ce2ab6e9d26b25591b4acfd49e5d0e7e17c
aisflat439 <fitzsimonsdevin@gmail.com>
2017-02-27 14:48:05 -0500

    long loop solution to merging arrays

--- february/2_challenge.js
+++ february/2_challenge.js
@@ -10,8 +10,20 @@ var ARRAY_ONE = [2, 9, 22, 97, 167, 204];
 var ARRAY_TWO = [3, 7, 10, 22, 98, 188, 203, 205, 209, 215];
 
 function mergeTwoArrays(arrayOne, arrayTwo){
-  var mergedArray = []
+  let mergedArray = []
+  let shorterArray = arrayOne.length < arrayTwo.length ? arrayOne : arrayTwo;
+  let longerArray = arrayOne.length >= arrayTwo.length ? arrayOne : arrayTwo;
+
+  let len = longerArray.length;
+  let k = 0;
+  for (let i = 0; i < len; i++){
+    while (shorterArray[k] <= longerArray[i]) {
+      mergedArray.push(shorterArray[k]);
+      k++;
+    }
+    mergedArray.push(longerArray[i]);
+  }
   return mergedArray;
 }
 
-mergeTwoArrays(ARRAY_ONE, ARRAY_TWO);
+console.log(mergeTwoArrays(ARRAY_ONE, ARRAY_TWO));

ec3f3a50ff40795eb34b61a5d63f4f89564233b8
aisflat439 <fitzsimonsdevin@gmail.com>
2017-03-01 11:02:27 -0500

    Possible solution.

---
+++ march/march_1st.js
@@ -0,0 +1,41 @@
+'use strict'
+
+// `[8, 1, 6, 3, 5, 7, 4, 9, 2] => true`
+// `[2, 7, 6, 9, 5, 1, 4, 3, 8] => true`
+// `[3, 5, 7, 8, 1, 6, 4, 9, 2] => false`
+// `[8, 1, 6, 7, 5, 3, 4, 9, 2] => false`
+const magicNumber = 15;
+
+function magicBoxSolver(magicBox) {
+  var mb = true;
+  Object.values(magicBox).forEach(row => {
+    // console.log(row);
+    let valueOfRow = row.reduce((a, b) => {
+      return a + b;
+    });
+    // console.log(valueOfRow);
+    if (valueOfRow !== magicNumber) {
+      mb = false;
+    }
+  });
+  return mb;
+}
+
+
+function MagicBox(array){
+  this.top = [array[0], array[1], array[2]];
+  this.middle = [array[3], array[4], array[5]];
+  this.bottom = [array[6], array[7], array[8]];
+  this.diagonalBT = [array[6], array[4], array[2]];
+  this.diagonalTB = [array[0], array[4], array[8]];
+}
+
+let mb1 = new MagicBox([8, 1, 6, 3, 5, 7, 4, 9, 2]);
+let mb2 = new MagicBox([2, 7, 6, 9, 5, 1, 4, 3, 8]);
+let mb3 = new MagicBox([3, 5, 7, 8, 1, 6, 4, 9, 2]);
+let mb4 = new MagicBox([8, 1, 6, 7, 5, 3, 4, 9, 2]);
+
+console.log(magicBoxSolver(mb1));
+console.log(magicBoxSolver(mb2));
+console.log(magicBoxSolver(mb3));
+console.log(magicBoxSolver(mb4));

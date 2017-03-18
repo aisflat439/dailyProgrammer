b64cbf8798e7b90a27e60f887a689c57f5fe5570
aisflat439 <fitzsimonsdevin@gmail.com>
2017-03-17 20:22:44 -0400

    march 16

--- march/16_march.js
+++ march/16_march.js
@@ -4,26 +4,24 @@ let testArray = [3, 55, 2, 78];
 let exampleArray = [34, 7, 38, 1];
 
 function sortAndIndexArray(array){
-  let arrayAsObj = {};
+  let holderArray = [];
+  let stringValue = "";
 
-  var len = array.length;
+  let len = array.length;
   for (let i = 0; i < len; i++) {
-    arrayAsObj[array[i]] = i + 1;
-  }
-  let value = createSentence(arrayAsObj);
-  console.log(value);
-  return arrayAsObj;
-}
-
-function createSentence(obj) {
-  let sentence = "";
-
-  for each (var item in obj) {
-    sentence += "its " item + "\n";
+    let pojo = { value: array[i], location: (i + 1) };
+    holderArray.push(pojo);
   }
 
-  return sentence;
+  for (let i = 0; i < len; i++) {
+    if (i != 0) { stringValue += '\n'; }
+    stringValue += `Space ${holderArray[i].location} held ${holderArray[i].value}`;
+  }
+
+  return stringValue;
 }
 
+console.log("Should hold 1/3, 2/55, 3/2, 4/78");
 console.log(sortAndIndexArray(testArray));
+console.log("Should hold 1/34, 2/7, 3/38, 4/1");
 console.log(sortAndIndexArray(exampleArray));

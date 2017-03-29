0e025aff11c33a26042e9c0c3fca6ae5e3ba7c9b
aisflat439 <fitzsimonsdevin@gmail.com>
2017-03-29 08:56:53 -0400

    29th challenge

---
+++ march/29_march.js
@@ -0,0 +1,33 @@
+'use strict'
+
+function mergeOverlappingAmounts( ...pairs ){
+  let overlappingValuesOnly = [];
+
+  pairs.forEach((pair, index, pairs) => {
+    if (index % 2 === 0) {
+      overlappingValuesOnly.push(comparePair(pair, pairs[index + 1]));
+    }
+  });
+
+  return overlappingValuesOnly;
+}
+
+function comparePair(setOne, setTwo) {
+  let newPair = [];
+
+  setOne = setOne.concat(setTwo);
+  setOne.sort(function compare(a, b) {
+    return a > b;
+  });
+  newPair.push(setOne[0]);
+  newPair.push(setOne[3]);
+
+  return newPair;
+}
+
+console.log("Should print [1,6] and [7,11]");
+console.log(mergeOverlappingAmounts( [1, 3], [2, 6], [8, 10], [7, 11] ));
+console.log("Should print [5,12]");
+console.log(mergeOverlappingAmounts( [5, 12], [8, 10] ));
+
+console.log(comparePair( [5, 12], [8, 10] ));

589c3b3e6205c1bd4eff3639c71c670f2229c6da
aisflat439 <fitzsimonsdevin@gmail.com>
2017-05-09 08:39:25 -0400

    initial commit of sum problem

---
+++ may/9_may.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function findSubsetsThatSumToZero(array) {
+
+}
+
+console.log(`Should be true`);
+console.log([-3, 1, 2]);
+console.log(`Should be true`);
+console.log([-98634, -86888, -48841, -40483, 2612, 9225, 17848, 71967, 84319, 88875]);
+console.log(`Should be false`);
+console.log([-3, 2, 7]);
+console.log(`Should be false`);
+console.log([-83314, -82838, -80120, -63468, -62478, -59378, -56958, -50061, -34791, -32264, -21928, -14988, 23767, 24417, 26403, 26511, 36399, 78055]);

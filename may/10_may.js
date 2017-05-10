51c5a5d1a467bdba439e2c22a364651372d3c4a3
aisflat439 <fitzsimonsdevin@gmail.com>
2017-05-10 08:57:29 -0400

    add start of 10 may prob

---
+++ may/10_may.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function translateRange(inputRange){
+  let translatedRange = inputRange.map((item, index, array) => {
+
+  });
+
+  return translateRange;
+}
+
+console.log(`Should print [1, 6, 14, 19, 21, 26]`);
+console.log(translateRange([1, 6, 4, 9, 1, 6]));
+console.log(`Should print [2, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 29]`);
+console.log(translateRange([2, 9..9, 9]));
+console.log(`Should print [5, 7, 10, 11, 19, 24]`);
+console.log(translateRange([5, 7, 0, 1, 9, 4]));

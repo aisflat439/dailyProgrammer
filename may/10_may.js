24c31f068e4b48d7bf2a1d2754d797776be5b995
aisflat439 <fitzsimonsdevin@gmail.com>
2017-05-10 09:29:50 -0400

    start solving problem, seems like I may be taking the long way.

--- may/10_may.js
+++ may/10_may.js
@@ -1,16 +1,29 @@
 'use strict';
 
 function translateRange(inputRange){
-  let translatedRange = inputRange.map((item, index, array) => {
+  let translatedRange = [];
 
+  inputRange.map((item, index, array) => {
+    if (typeof translatedRange === undefined || translatedRange.length === 0) {
+      translatedRange.push(item);
+      return;
+    }
+    if (item > translatedRange[translatedRange.length - 1]) {
+      translatedRange.push(item);
+    } else {
+      item += 10;
+      // while (item > translatedRange[translatedRange.length - 1]) {
+      // }
+      translatedRange.push(item);
+    }
   });
 
-  return translateRange;
+  return translatedRange;
 }
 
 console.log(`Should print [1, 6, 14, 19, 21, 26]`);
 console.log(translateRange([1, 6, 4, 9, 1, 6]));
-console.log(`Should print [2, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 29]`);
-console.log(translateRange([2, 9..9, 9]));
+// console.log(`Should print [2, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 29]`);
+// console.log(translateRange([2, 9..9, 9]));
 console.log(`Should print [5, 7, 10, 11, 19, 24]`);
 console.log(translateRange([5, 7, 0, 1, 9, 4]));

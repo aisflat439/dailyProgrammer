6c6ef09dd9b5ada83946734530e13bf154fd74c7
aisflat439 <fitzsimonsdevin@gmail.com>
2017-06-30 08:57:02 -0400

    start 30 june

--- june/26_june.js
+++ june/26_june.js
@@ -3,32 +3,68 @@
 function getBowlingScore(scoreArray) {
   let score = 0,
       frame = 1,
-      previousFrameIsStrike = false;
+      allFrames = [];
 
-  scoreArray.forEach((item) => {
-    if (item === 'X') {
-      if (previousFrameIsStrike){
-        score += 20;
-      } else {
-        score += 10;
-      }
-      frame++;
-      previousFrameIsStrike = true;
-    }
-
-    if (item === 'XXX') {
-      if (previousFrameIsStrike){
-        score += 40;
-      } else {
-        score += 30;
-      }
-      frame++;
-      previousFrameIsStrike = true;
-    }
+  scoreArray.forEach((item, index) => {
+    let frame = new Frame(index + 1, item);
+    allFrames.push(frame)
   });
 
-  return `Score: ${score}, numFrames: ${frame}`;
+  return allFrames;
 }
+
+function Frame(frameNumber, score) { //, firstPreviousFrame, secondPreviousFrame) {
+  this.score = score;
+  // this.previousFrame = firstPreviousFrame.getScore();
+  // this.secondPreviousFrame = secondPreviousFrame.getScore();
+  this.isSpare = false;
+  this.isStrike = false;
+  this.frameNumber = frameNumber;
+
+  function getScore() {
+    return this.score;
+  }
+
+  function getFrameNumber() {
+    return this.frameNumber;
+  }
+}
+
+
+
+
+// function getBowlingScore(scoreArray) {
+//   let score = 0,
+//       frame = 1,
+//       previousFrameIsSpare = false,
+//       currentFrameIsSpare = false,
+//       previousFrameIsStrike = false,
+//       currentFrameIsStrike = false;
+//
+//   scoreArray.forEach((item) => {
+//     if (item === 'X') {
+//       if (previousFrameIsStrike){
+//         score += 20;
+//       } else {
+//         score += 10;
+//       }
+//       frame++;
+//       previousFrameIsStrike = true;
+//     }
+//
+//     if (item === 'XXX') {
+//       if (previousFrameIsStrike){
+//         score += 40;
+//       } else {
+//         score += 30;
+//       }
+//       frame++;
+//       previousFrameIsStrike = true;
+//     }
+//   });
+//
+//   return `Score: ${score}, numFrames: ${frame}`;
+// }
 
 console.log(`Next line should be 300`);
 console.log(getBowlingScore(['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'XXX']));
---
+++ june/30_june.js
@@ -0,0 +1,8 @@
+'use strict';
+
+function findTwoSumParts() {
+  return 'omggmo'
+}
+
+console.log('should return 13');
+console.log(findTwoSumParts([2, 5, 76, 8], 13));

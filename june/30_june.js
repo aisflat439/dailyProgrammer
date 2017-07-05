ac6d69afdb4bb3871ec4b88232953c495c81542a
aisflat439 <fitzsimonsdevin@gmail.com>
2017-07-05 09:36:39 -0400

    july 5

--- june/30_june.js
+++ june/30_june.js
@@ -1,7 +1,17 @@
 'use strict';
 
-function findTwoSumParts() {
-  return 'omggmo'
+function findTwoSumParts(numberSet, target) {
+  let targetSum = target,
+      firstNumber = 0,
+      secondNumber = 0;
+
+  numberSet.forEach((item, index, arr) => {
+    for (let i = 0; i < arr.length; i++) {
+      
+    }
+  });
+
+  return 'derp';
 }
 
 console.log('should return 13');

9be99c276908fb4aeb9bb3c2cbaed009a264180c
aisflat439 <fitzsimonsdevin@gmail.com>
2017-07-13 09:55:52 -0400

    failure of a solution

--- july/13_july.js
+++ july/13_july.js
@@ -1,5 +1,27 @@
 function printSquare(number) {
-  return number;
+  let numberSquared = number * number,
+      values = [],
+      testValues = [],
+      printedLine = '',
+      pairs = {};
+
+  for (let i = 1; i <= numberSquared; i++) {
+    printedLine += ' ' + i;
+    if (i % number === 0) {
+      printedLine += '\n';
+    }
+    values.push(i);
+  }
+
+  values.forEach((item, index, array) => {
+    for (let i = 0; i < number; i++) {
+      if (item % number === i) {
+        pairs[item] = i;
+      }
+    }
+  });
+
+  return printedLine;
 }
 
 console.log(printSquare(5));
